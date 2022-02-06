126aeabf708c78d471347a42e80461444dd20203
ameliejyc <amelie.chan@gmail.com>
2022-02-06 11:34:38 +0100

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -51965,7 +51965,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51475" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51517" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
